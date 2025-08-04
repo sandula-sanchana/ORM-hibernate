873e83620df7f1c21fd2379fa216149fc22dedbb
sandula-sanchana <sandulasanchana@gmail.com>
2025-08-04 14:11:12 +0530

    first commit

---
+++ src/main/java/Entity/Customer.java
@@ -0,0 +1,40 @@
+package Entity;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.Table;
+
+@Entity
+@Table(name="customer_table")
+public class Customer {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public Customer() {
+
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
